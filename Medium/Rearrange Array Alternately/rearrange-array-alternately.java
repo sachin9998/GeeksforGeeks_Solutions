14c8c4d550628d5d957acb2db1c076eb091601fd
Sachin Alam <sachinalam4216@gmail.com>
2023-01-19 17:44:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rearrange Array Alternately/rearrange-array-alternately.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+        // Your code here
+        int maxIndex = n - 1;
+        int minIndex = 0;
+        
+        long maxElement = arr[n - 1] + 1;
+        
+        for(int i = 0; i < n; i++) {
+            if(i%2 == 0) {
+                
+                arr[i] = (arr[maxIndex] % maxElement) * maxElement + arr[i];
+                maxIndex--;
+            } else {
+                
+                arr[i] = (arr[minIndex] % maxElement) * maxElement + arr[i];
+                minIndex++;
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            arr[i] = arr[i] / maxElement;
+        }
+        
+    }
+    
+}
+
+
