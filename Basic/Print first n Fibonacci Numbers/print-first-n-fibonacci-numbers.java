24f3dafeba3288b061c5e98fc3b11e5ad67923a3
Sachin Alam <sachinalam4216@gmail.com>
2023-05-14 16:48:35 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Print first n Fibonacci Numbers/print-first-n-fibonacci-numbers.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total count of testcases
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    //taking total number of elements
+		    int n=sc.nextInt();
+		    
+		    //calling printFibb() method
+		    long[] res = new Solution().printFibb(n);
+		    
+		    //printing the elements of the array
+		    for (int i = 0; i < res.length; i++)
+		        System.out.print (res[i]+" ");
+		    System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to return list containing first n fibonacci numbers.
+    public static long[] printFibb(int n) 
+    {
+        //Your code here
+        long ans[] = new long[n];
+        
+        long first = 1;
+        long second = 1;
+        
+        for(int i = 0; i < n; i++) {
+            ans[i] = first;
+            long next = first + second;
+            first = second;
+            second = next;
+        }
+        
+        return ans;
+        
+        
+    }
+}
