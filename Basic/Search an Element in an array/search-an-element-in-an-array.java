ef7f42545fd536e9ad6c7e1dcc89f8a5acc464e3
Sachin Alam <sachinalam4216@gmail.com>
2023-08-30 23:42:31 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Search an Element in an array/search-an-element-in-an-array.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int sizeOfArray = Integer.parseInt(read.readLine());
+            int arr[] = new int[sizeOfArray];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            for(int i = 0; i < sizeOfArray; i++)
+              arr[i] = Integer.parseInt(st[i]);
+              
+            int x = Integer.parseInt(read.readLine());
+            
+            Solution obj = new Solution();
+            
+            System.out.println(obj.search(arr, sizeOfArray, x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+                        // Parameters
+    static int search(int arr[], int N, int X)
+    {
+        
+        // Your code here
+        int ans = -1;
+        
+        for(int i = 0; i < N; i++) {
+           // 1 2 3 4 5 // x == 8
+            if(arr[i] == X) {
+                return i;
+            }
+        }
+        
+        return ans;
+        
+    }
+}
