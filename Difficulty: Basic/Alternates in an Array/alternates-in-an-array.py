665dc576c5b2830d2cb8fb795d86a2c9a764ae02
Sachin Alam <sachinalam4216@gmail.com>
2026-04-16 23:01:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Alternates in an Array/alternates-in-an-array.py
+++ Difficulty: Basic/Alternates in an Array/alternates-in-an-array.py
@@ -1,9 +1,4 @@
 class Solution:
     def getAlternates(self, arr):
-        n = len(arr)
-        res = []
-        
-        for i in range(0, n, 2):
-            res.append(arr[i])
-            
-        return res
+        # Code Here
+        return arr[::2]
