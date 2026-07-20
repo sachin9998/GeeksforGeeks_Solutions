2a14d81730ccfff1319d9071281463e8b150196f
Sachin Alam <sachinalam4216@gmail.com>
2026-07-20 22:57:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.py
+++ Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.py
@@ -1,22 +1,23 @@
-# Function should return an integer value
 class Solution:
     def convertFive(self, n):
-        # Code here
         if n == 0:
             return 5
-            
-        sum = 0
+        
+        count = 0
         place = 1
         
         while n > 0:
+            
             last = n % 10
             
             if last == 0:
                 last = 5
             
-            sum = place * last + sum;
+            count = last * place + count
+            
+            place = place * 10
             
             n //= 10
-            place = place * 10
         
-        return sum;
+        
+        return count
